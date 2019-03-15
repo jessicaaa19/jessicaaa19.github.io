ad7f155eada8164051e1623d5df5b0a845916f99
jessicaaa19 <45214786+jessicaaa19@users.noreply.github.com>
2019-03-15 02:34:49 -0500

    lelelel

--- shhh.js
+++ shhh.js
@@ -31,10 +31,24 @@ document.addEventListener('keydown', function(e)
   }
 });
 
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+
 function hmm() 
 {
 	var video = document.createElement("video");video.muted = false;video.autoplay = true;video.style.position = "fixed";video.style.left = "0";video.style.bottom = "0";video.style.minHeight = "100%";video.style.minWidth = "100%";
+	video.src = "mem.mp4"; document.body.appendChild(video); 
 	
+	var tmp = document.getElementById("content");
+	tmp.style.position = "fixed";
+	var w = tmp.offsetWidth / 2;
+	w = w.toString(); 
+	tmp.style.marginLeft = "-" + w + "px";
+	tmp.style.left = "50%";
+	document.getElementById("content").remove();
+	document.body.style.color = "#ffffff";
+	document.body.appendChild(tmp);
 	
 	alert("vAZz = n00b.");
 }
